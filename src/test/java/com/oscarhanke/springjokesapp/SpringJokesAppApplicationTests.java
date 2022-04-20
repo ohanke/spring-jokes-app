4ca48018c43217b741219aa32e5a6bee452a43a5
ohanke <steamoha@wp.pl>
2022-04-20 20:10:54 +0200

    Created Spring Boot Project. Closes #1.

---
+++ src/test/java/com/oscarhanke/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
